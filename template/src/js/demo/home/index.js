ae94281f0ff4561655c9d171e0d5bdce3f0f5c78
evio <shenyunjie@51zhangdan.com.cn>
2016-03-04 00:06:04 +0800

    update

--- template/src/js/demo/home/index.js
+++ template/src/js/demo/home/index.js
@@ -1,7 +1,6 @@
 var Browser = module.exports = function(app){
     var homeBrowser = app.$browser('home');
     app.$use(homeBrowser);
-    app.$use(function(){ this.$headbar.$emit('reset'); });
     Browser.info(homeBrowser);
     Browser.index(homeBrowser);
 }
@@ -10,6 +9,7 @@ Browser.index = function(browser){
     browser.$active(function(){
         this.$render('index', {
             before: function(){
+                this.$headbar.$reset();
                 this.$headbar.center.text = 'Simplize Demo';
                 this.$headbar.class = 'white';
                 this.$toolbar.status = false;
@@ -22,6 +22,7 @@ Browser.info = function(browser){
     browser.$active('/info', function(){
         this.$render('info', {
             before: function(){
+                this.$headbar.$reset();
                 this.$headbar.left.icon = '<i class="fa fa-angle-left">';
                 this.$headbar.left.text = 'Back';
                 this.$headbar.left.fn = function(){ history.back(); };
